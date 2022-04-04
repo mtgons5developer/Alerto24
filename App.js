018be70df09fdf548b4bce86c0328396eba4e622
Enam <surelastmail@gmail.com>
2022-04-05 00:33:32 +0600

    Firebase Login Done

--- App.js
+++ App.js
@@ -3,11 +3,28 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import LoginScreen from "./app/screens/LoginScreen";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
+      <Stack.Navigator >
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="LoginScreen"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Dashboard"
+          component={AppNavigator}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+    // <LoginScreen/>
   );
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -5,19 +5,48 @@ import * as Yup from "yup";
 import Screen from "../components/Screen";
 import { Form, FormField, SubmitButton } from "../components/forms";
 
+import {
+  ToastAndroid,
+  Platform,
+  AlertIOS,
+} from 'react-native';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEjcCPhzeEtj9Tp_MDZM3waHxM97JeahM",
+  authDomain: "schooapp2022.firebaseapp.com",
+  databaseURL: "https://schooapp2022-default-rtdb.firebaseio.com",
+  projectId: "schooapp2022",
+  storageBucket: "schooapp2022.appspot.com",
+  messagingSenderId: "26197024544",
+  appId: "1:26197024544:web:209e7d360fb391cc3ac145",
+  measurementId: "G-74SZW3KGPH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(4).label("Password"),
 });
 
-function LoginScreen(props) {
+function LoginScreen({navigation}) {
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
       <Form
         initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => onFormSubmit(values)}
         validationSchema={validationSchema}
       >
         <FormField
@@ -42,6 +71,24 @@ function LoginScreen(props) {
       </Form>
     </Screen>
   );
+  function onFormSubmit(values) {
+    // alert('code changed');
+
+    signInWithEmailAndPassword(auth, values.email, values.password).then(function (response) {
+      // alert(JSON.stringify(response))
+      navigation.navigate('Dashboard')
+
+    }).catch(function (err) {
+      const msg = 'Invalid Email or Password. Please Try Again';
+      if (Platform.OS === 'android') {
+        ToastAndroid.show(msg, ToastAndroid.SHORT)
+      } else {
+        // AlertIOS.alert(msg);
+        alert(msg)
+      }
+
+    });
+  }
 }
 
 const styles = StyleSheet.create({
