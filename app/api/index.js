b8484045eaf88d6fb0cc50f77d799a92a0eac076
Mudassir_Nadeem <102925482+MudassirSh4003@users.noreply.github.com>
2022-04-17 03:24:48 +0500

    commit

---
+++ app/api/index.js
@@ -0,0 +1,8 @@
+export const Sign_up = {
+    BASE_URL: 'https://dataxphilippines.com/api/register',
+    
+}
+export const Sign_in = {
+    BASE_URL: 'https://dataxphilippines.com/api/login',
+    
+}
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,29 +1,64 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React,{useState} from "react";
+import { StyleSheet,Text,TouchableOpacity } from "react-native";
 import * as Yup from "yup";
-
+import { Sign_up } from "../api";
 import Screen from "../components/Screen";
 import { Form, FormField, SubmitButton } from "../components/forms";
 
-const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(4).label("Password"),
-});
 
-function RegisterScreen() {
+
+
+const RegisterScreen=()=> {
+
+  const [Name, setName] = useState('')
+  const [Email, setEmail] = useState('')
+  const [Password, setPassword] = useState('')
+
+ const SignUpSubmit = () => {
+		fetch(Sign_up.BASE_URL, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				Name: Name,
+				Email: Email,
+				Password: Password,
+				
+			})
+		})
+			.then((response) => response.json())
+			.then((json) => {
+				if (json.status == true) {
+					this.props.navigation.navigate('Login');
+
+					// const data = performTimeConsumingTask();
+				} else {
+					alert('SignUp Failed');
+					console.log(json.errors.Name);
+					console.log(json.errors.Email);
+					console.log(json.errors.Password);
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+
   return (
     <Screen style={styles.container}>
       <Form
         initialValues={{ name: "", email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
-        validationSchema={validationSchema}
       >
         <FormField
           autoCorrect={false}
           icon="account"
           name="name"
           placeholder="Name"
+          handleChange={(name)=>{setName(name)}}
         />
         <FormField
           autoCapitalize="none"
@@ -33,6 +68,8 @@ function RegisterScreen() {
           name="email"
           placeholder="Email"
           textContentType="emailAddress"
+          handleChange={(email)=>{setEmail(email)}}
+  
         />
         <FormField
           autoCapitalize="none"
@@ -42,8 +79,9 @@ function RegisterScreen() {
           placeholder="Password"
           secureTextEntry
           textContentType="password"
+          handleChange={(pass)=>{setPassword(pass)}}
         />
-        <SubmitButton title="Register" />
+       <SubmitButton title={'Submit'} handleSubmit={()=>{SignUpSubmit()}}/>
       </Form>
     </Screen>
   );
@@ -53,6 +91,12 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
+  button:{
+    borderRadius:20,
+    borderWidth:1,
+    alignContent:'center',
+    justifyContent:'center'
+  }
 });
 
 export default RegisterScreen;
